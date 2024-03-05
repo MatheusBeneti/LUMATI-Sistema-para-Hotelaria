b91536d23c778927ce7205721affda82690e264c
MatheusBeneti <matheusbeneti3@gmail.com>
2024-03-05 17:32:07 -0300

    mudança de rota

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")
+	@GetMapping("/")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Duro tem que andar duro!";
 	}
 
 	public static void main(String[] args) {
