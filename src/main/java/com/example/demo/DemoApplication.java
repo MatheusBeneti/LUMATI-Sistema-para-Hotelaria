f97f672734114c9d85af65e96ddaefb86540118b
Beneti <matheusbeneti2@gmail.com>
2024-03-05 14:40:54 -0300

    teste mudandoo codigo da main sem trust

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class MeuAplicativoSpringBoot {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeuAplicativoSpringBoot.class, args);
+    }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Bem-vindo ao meu aplicativo Spring Boot!";
+    }
 }
+
