f56e05c98bcf23d5bd238d1e9a2252914ba594c3
2xTi <treisalmeida@gmail.com>
2024-03-05 21:08:57 -0300

    banco de dados conectado

---
+++ src/main/java/com/example/springboot/models/RoomModel.java
@@ -0,0 +1,50 @@
+package com.example.springboot.models;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table (name = "TB ROOMS")
+public class RoomModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idRoom;
+    private String clientName;
+    private String inDate;
+    private String outDate;
+
+    public UUID getIdRoom() {
+        return idRoom;
+    }
+
+    public void setIdRoom(UUID idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getInDate() {
+        return inDate;
+    }
+
+    public void setInDate(String inDate) {
+        this.inDate = inDate;
+    }
+
+    public String getOutDate() {
+        return outDate;
+    }
+
+    public void setOutDate(String outDate) {
+        this.outDate = outDate;
+    }
+}
